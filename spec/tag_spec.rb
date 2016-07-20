745ed29137ff94fecb2916f4da9a663a854bba63
Alvin Ng and Caleb Stokka <alvin+caleb@>
2016-07-20 10:13:01 -0700

    add recipe spec for the Tag class

---
+++ spec/tag_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Tag do
+  describe 'recipe' do
+    it 'returns the recipe associated with the tag' do
+      quiche = Recipe.create({:name => 'quiche'})
+      croissant = Recipe.create({:name => 'croissant'})
+      breakfast = Tag.create({:name => 'breakfast'})
+      french = Tag.create({:name => 'French'})
+      french.update({:recipe_ids => [quiche.id, croissant.id]})
+      expect(french.recipes).to(eq([quiche, croissant]))
+    end
+  end
+end
