919a5372270bc6a134d2b67e35c7256b86ab9626
Alvin Ng and Caleb Stokka <alvin+caleb@>
2016-07-20 14:53:23 -0700

    add tags to recipes in the front end

--- app.rb
+++ app.rb
@@ -17,6 +17,30 @@ post '/recipes' do
   name = params.fetch 'name'
   ingredient = params.fetch 'ingredient'
   instruction = params.fetch 'instruction'
-  recipe = Recipe.create({name: name, instruction: instruction, ingredient: ingredient})
+  @recipe = Recipe.create({name: name, instruction: instruction, ingredient: ingredient})
   redirect '/recipes'
+end
+
+get '/tags' do
+  @tags = Tag.all
+  erb :tags
+end
+
+post '/tags' do
+  name = params.fetch 'name'
+  tag = Tag.create({name: name})
+  redirect '/tags'
+end
+
+get '/recipe/:id' do
+  @tags = Tag.all
+  @recipe = Recipe.find(params.fetch('id'))
+  erb :recipe
+end
+
+patch '/recipe/:id' do
+  tag = Tag.find(params.fetch('tag'))
+  @recipe = Recipe.find(params.fetch('id'))
+  @recipe.tags.push(tag)
+  redirect("/recipe/".concat(@recipe.id().to_s()))
 end
