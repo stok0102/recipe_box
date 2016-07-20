b16c59d558292e0b4dd2153d59af9e07399bcacf
Alvin Ng and Caleb Stokka <alvin+caleb@>
2016-07-20 10:29:24 -0700

    removes a tag from a recipe

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -10,4 +10,15 @@ describe Recipe do
       expect(recipe.tags).to(eq([breakfast, french]))
     end
   end
+
+  describe 'remove tag' do
+    it "removes a tag from a recipe" do
+      breakfast = Tag.create({:name => 'breakfast'})
+      french = Tag.create({:name => 'French'})
+      recipe = Recipe.create({:name => 'quiche', :tag_ids => [breakfast.id, french.id]})
+      expect(recipe.tags).to(eq([breakfast, french]))
+      recipe.tags.destroy(breakfast)
+      expect(recipe.tags).to(eq([french]))
+    end
+  end
 end
